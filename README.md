f21914a01df4664ad5207526ff0dcf9aca31a7d4
Mike Milano <coder1@gmail.com>
2018-06-11 19:40:36 -0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,33 @@
+xrpsign
+=======
+
+A simple package for signing XRP transactions.
+
+## Installation
+
+```
+npm install xrpsign
+```
+
+## Usage
+
+```
+const sign = require('xrpsign');
+
+// tx can be an object or the JSON equivalent.
+let tx = {
+    TransactionType: 'Payment',
+    Account: 'rQBnNY5w5cALHbMaue2VefSzuBfxafwqp9',
+    Fee : '12',
+    Destination: 'rnQ1WgToG2RL9Fjmofif9ixYVgJTi6BLas',
+    Amount: '1000000',
+    Sequence: 3
+}
+
+// signedTx is now a transaction object with the signed data.
+let signedTx = sign(tx);
+```
+
+## Acknowledgements
+
+Big thanks to [@WietseWind](https://twitter.com/WietseWind) for help on this one. 
---
+++ index.js
@@ -0,0 +1,26 @@
+const RippleKeypairs = require('ripple-keypairs');
+const RippleBinaryCodec = require('ripple-binary-codec');
+const RippleHashes = require('ripple-hashes');
+
+const sign = (tx, secret) => {
+
+    if (typeof tx === 'string') {
+        tx = JSON.parse(tx);
+    }
+
+    let txBlob;
+    let txId;
+    let keypair = RippleKeypairs.deriveKeypair(secret.trim());
+    tx.SigningPubKey = keypair.publicKey;
+    tx.TxnSignature = RippleKeypairs.sign(RippleBinaryCodec.encodeForSigning(tx), keypair.privateKey);
+
+    txBlob = RippleBinaryCodec.encode(tx);
+    txId = RippleHashes.computeBinaryTransactionHash(txBlob);
+
+    return {
+        tx_blob: txBlob,
+        tx_id: txId
+    };
+};
+
+module.exports = sign;
